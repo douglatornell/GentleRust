24806badbc5fded9dfeb7da96bb080415a5f55f9
Doug Latornell <djl@douglatornell.ca>
2019-01-13 14:01:56 -0800

    Add iterator exercises.

---
+++ iter3.rs
@@ -0,0 +1,16 @@
+// iter3.rs
+fn main() {
+    let arr = [10, 20, 30];
+    for i in arr.iter() {
+        println!("{}", i);
+    }
+
+    let slice = &arr;
+    for i in slice {
+        println!("{}", i);
+    }
+
+    for i in &arr {
+        println!("{}", i);
+    }
+}
